a90bedaab524be7ddaaa4c69bcc2f95abfea29b8
Michał Kowalczyk <michal.p.kowalczyk@gmail.com>
2021-11-22 19:53:37 +0100

    Update swiper-nav.tsx

--- codeebo-tools/src/components/swiper-nav/swiper-nav.tsx
+++ codeebo-tools/src/components/swiper-nav/swiper-nav.tsx
@@ -3,18 +3,16 @@ import "./swiper-nav.scss";
 
 interface Props {
   className: string;
-  classNamePrev: string;
-  classNameNext: string;
 }
 
 const SwiperNav: React.FC<Props> = (props: Props) => {
-  const { className, classNamePrev, classNameNext } = props;
+  const { className } = props;
   return (
     <div className={`swiper-nav ${className}`}>
-      <div className={`nav-prev ${classNamePrev}`} data-aos="slide-right" aria-label="Poprzedni slajd" role="button">
+      <div className={`nav-prev ${className}-prev`} data-aos="slide-right" aria-label="Poprzedni slajd" role="button">
         &lt;
       </div>
-      <div className={`nav-next ${classNameNext}`} data-aos="slide-left" aria-label="Następny slajd" role="button">
+      <div className={`nav-next ${className}-next`} data-aos="slide-left" aria-label="Następny slajd" role="button">
         &gt;
       </div>
     </div>
